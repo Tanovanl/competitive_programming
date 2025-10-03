17f8af25ab20eeec5eb382f1450bf028e0d45aea
tanovanlerberghe <tano.vanlerberghe@student.howest.be>
2025-10-03 16:53:17 +0200

    fix oefening 1

--- src/main/java/question1/Main.java
+++ src/main/java/question1/Main.java
@@ -1,61 +1,62 @@
-//package question1;
-//
-//
-//import java.nio.file.*;
-//import java.io.IOException;
-//import java.util.List;
-//
-//public class Main {
-//    public static void main(String[] args) throws IOException {
-//       List<String> lines = Files.readAllLines(Paths.get("src/invoer.txt"));
-//
-//
-//        int aantalNetwerken = Integer.parseInt(lines.get(0));
-//        int[][][] netwerken = new int[1][210][1000];
-//        int currentLine = 1;
-//        int value = Integer.parseInt(lines.get(currentLine));
-//
-//        for (int i = 0; i < aantalNetwerken; i++){
-//            int counter = 0;
-//            for (int j = value; j < currentLine + value; j++){
-//
-//                netwerken[1][i][counter] = Integer.parseInt(lines.get(j));
-//                counter++;
-//            }
-//        }
-//
-//
-//        int line = 1;
-//
-//
-//
-//
-//
-//
-//        for (int i = 0; i < aantalNetwerken; i++){
-//            int shortestWay = 10000000;
-//            for (int j = 0; j < aantalPaden; j++){
-//                for(int x = 0; x < aantalPaden; x++){
-//                    int length = 0;
-//                    if (x != j ){
-//                        if (array[j][0] <= array[x][0]){
-//                            length = array[x][0];
-//                        } else {
-//                            length = array[j][0] + array[x][0];
-//
-//                        }
-//                        length += array[j][1];
-//
-//                        if (length < shortestWay){
-//                            shortestWay = length;
-//                        }
-//                    }
-//                }
-//            }
-//            System.out.println(shortestWay);
-//        }
-//
-//    }
-//
-//}
-//
+package question1;
+
+
+import java.nio.file.*;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       List<String> lines = Files.readAllLines(Paths.get("src/invoer.txt"));
+        int index = 0;
+
+        int amount = Integer.parseInt(lines.get(index++));
+
+        for (int i = 0; i < amount; i++){
+            int paths = Integer.parseInt(lines.get(index++));
+            int[][] allPaths = new int[paths][2];
+            for (int j = 0; j < paths; j++){
+                String[] parts = lines.get(index++).split(" ");
+                allPaths[j][0] = Integer.parseInt(parts[0]);
+                allPaths[j][1] = Integer.parseInt(parts[1]);
+            }
+            System.out.println(calculate(allPaths));
+        }
+
+        int[][] voorbeeld = {
+                {13, 17},
+                {19, 11},
+                {16, 14},
+                {15, 15}
+        };
+
+        // System.out.println(calculate(voorbeeld));
+
+    }
+
+    public static int calculate(int[][] input){
+        int shortestWay = 10000;
+        for (int j = 0; j < input.length; j++){
+            for(int x = 0; x < input.length; x++){
+                int length = 0;
+                if (x != j ){
+                    if (input[j][0] <= input[x][0]){
+                        length = input[x][0];
+                    } else {
+                        length = input[j][0] + input[x][0];
+
+                    }
+                    length += input[j][1];
+
+                    if (length < shortestWay){
+                        shortestWay = length;
+                    }
+                }
+            }
+        }
+        return shortestWay;
+    }
+
+}
+
